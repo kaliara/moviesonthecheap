99ba977a4770e871ecac1ce66a36ce4b02847e49
Matthew Kaliara <matthew@patentspark.com>
2013-09-20 04:51:34 -0400

    removed commented code

--- common/app.js
+++ common/app.js
@@ -13,11 +13,3 @@ angular.module('movieApp').
     $routeProvider.when('/checkout', {templateUrl: 'purchase/new.html', controller: 'PurchaseController'});
     $routeProvider.otherwise({redirectTo: 'home.html'});
   }]);
-
-
-// angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-//   config(['$routeProvider', function($routeProvider) {
-//     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-//     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-//     $routeProvider.otherwise({redirectTo: '/view1'});
-//   }]);
--- common/filters.js
+++ common/filters.js
@@ -1,19 +1,3 @@
 'use strict';
 
 /* Filters */
-
-movieApp.filter('durations', function(){
-  return function(duration){
-    switch(duration) {
-      case 1:
-        return "30 Minutes";
-      case 2:
-        return "1 Hour";
-      case 3:
-        return "Half Day";
-      case 4:
-        return "Full Day";
-    }
-  }
-  
-})
